b56d31ad0100ac9322b303665a7972791b3ebb78
Brannon Dorsey <brannon@brannondorsey.com>
2017-03-07 19:01:17 -0600

    Breakout feature extraction function to utils.py. Add analyze_generated_midi_with_tensorboard.ipynb

--- sandbox/python/utils.py
+++ sandbox/python/utils.py
@@ -2,6 +2,7 @@ import dill as pickle
 import numpy as np
 import time, numbers, os, pdb
 import shutil
+from music21 import *
 
 def query_dict_array(arr, query):
     
@@ -86,3 +87,74 @@ def create_symlink_dir(paths, dirname, file_limit_per_track=None):
                     continue
                 os.symlink(os.path.join(path, filename), os.path.join(dirname, filename))
     print('found {} duplicate midi files'.format(dupes))
+
+# extract features from a midi file using music21 + jSymbolic
+def extract_features(midi_path):
+
+    try:
+        s = converter.parse(midi_path)
+    except:
+        return None
+    
+    # for a description of features, see section 5 of the jSymbolic paper
+    # http://jmir.sourceforge.net/publications/MA_Thesis_2004_Bodhidharma.pdf
+    feats = [
+        # instrument features-----------------------------------
+#         features.jSymbolic.PitchedInstrumentsPresentFeature,
+#         features.jSymbolic.NotePrevalenceOfPitchedInstrumentsFeature,
+        features.jSymbolic.NumberOfPitchedInstrumentsFeature,
+        features.jSymbolic.StringKeyboardFractionFeature,
+        features.jSymbolic.AcousticGuitarFractionFeature,
+        features.jSymbolic.ElectricGuitarFractionFeature,
+        features.jSymbolic.ViolinFractionFeature,
+        features.jSymbolic.SaxophoneFractionFeature,
+        features.jSymbolic.BrassFractionFeature,
+        features.jSymbolic.WoodwindsFractionFeature,
+        features.jSymbolic.OrchestralStringsFractionFeature,
+        features.jSymbolic.StringEnsembleFractionFeature,
+        features.jSymbolic.ElectricInstrumentFractionFeature,
+        # rythm features----------------------------------------
+        features.jSymbolic.InitialTempoFeature,
+        features.jSymbolic.InitialTimeSignatureFeature,
+        features.jSymbolic.NoteDensityFeature,
+        features.jSymbolic.AverageNoteDurationFeature,
+        features.jSymbolic.AverageTimeBetweenAttacksFeature,
+        features.jSymbolic.VariabilityOfTimeBetweenAttacksFeature,
+        features.jSymbolic.AverageTimeBetweenAttacksForEachVoiceFeature,
+        features.jSymbolic.AverageVariabilityOfTimeBetweenAttacksForEachVoiceFeature,
+        features.jSymbolic.ChangesOfMeterFeature,
+        features.jSymbolic.MaximumNoteDurationFeature,
+        features.jSymbolic.MinimumNoteDurationFeature,
+        # pitch statistics features-----------------------------
+        features.jSymbolic.MostCommonPitchPrevalenceFeature,
+        features.jSymbolic.MostCommonPitchClassPrevalenceFeature,
+        features.jSymbolic.PitchVarietyFeature,
+        features.jSymbolic.PitchClassVarietyFeature,
+        features.jSymbolic.RangeFeature,
+        features.jSymbolic.ImportanceOfBassRegisterFeature,
+        features.jSymbolic.ImportanceOfMiddleRegisterFeature,
+        features.jSymbolic.ImportanceOfHighRegisterFeature,
+        features.jSymbolic.MelodicIntervalHistogramFeature,
+        features.jSymbolic.BasicPitchHistogramFeature,
+        features.jSymbolic.PitchClassDistributionFeature,
+        features.jSymbolic.QualityFeature, # mode
+#         features.jSymbolic.GlissandoPrevalenceFeature,
+#         features.jSymbolic.VibratoPrevalenceFeature,
+        # melody features---------------------------------------
+        features.jSymbolic.MelodicIntervalHistogramFeature,
+        features.jSymbolic.AmountOfArpeggiationFeature,
+        features.jSymbolic.RepeatedNotesFeature,
+        features.jSymbolic.ChromaticMotionFeature,
+        features.jSymbolic.StepwiseMotionFeature,
+        features.jSymbolic.MelodicThirdsFeature,
+        features.jSymbolic.MelodicFifthsFeature,
+        features.jSymbolic.MelodicTritonesFeature,
+        features.jSymbolic.MelodicOctavesFeature,
+        features.jSymbolic.DirectionOfMotionFeature
+    ]
+    
+    ds = features.DataSet(classLabel=midi_path)
+    ds.addData(s)
+    ds.addFeatureExtractors(feats)
+    ds.process()
+    return (ds.getFeaturesAsList(), ds.getAttributeLabels())
